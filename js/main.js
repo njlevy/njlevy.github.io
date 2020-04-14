420779c6216ea2bee3dc40d2fece9f4f7b646d88
njlevy <njlevy@gmail.com>
2020-04-15 02:50:06 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// $( document ).ready(function() {
+// var video;
+
+
+
+// window.addEventListener('click', function(e){  
+
+// if ($('html').hasClass( "lity-active" )){
+// video = $('.lity-content video');
+
+//   if ($('.lity-container').get(0).contains(e.target)){
+
+//  }
+//  else {
+//   		// alert('hey');
+ 	
+// video.get(0).pause();
+// video.get(0).currentTime = 0;  
+// }
+
+
+//  	}
+
+// });
+
+
+
+
+// $("body").keydown(function(e){
+//  var code = e.keyCode || e.which;
+//  if(code == 27) { 
+//  	alert('hey');
+//  	video = $('.lity-content video');
+
+// video.get(0).pause();
+// video.get(0).currentTime = 0;
+
+//  }
+// });
+
+// });
+
+
+
+// function playSelected(x){
+// let str = x;
+
+// str = str.substring(1);
+
+// /*
+// 	Output: ello
+// */
+// document.getElementById(str).play();
+
+
+
+// }
+
+
+
+
